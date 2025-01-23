f2e8a8194b8dd78c87c4b3d6dd1960ed1bff3105
Ajmal T A <111681642+AJMALAJU3@users.noreply.github.com>
2025-01-23 19:13:13 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const serverApi = 'https://webrtc-trial.vercel.app/api/';
+const serverApi = 'https://webrtc-trial.vercel.app/api';
 
 export const getRoomExists = async (roomId) => {
     const response = await axios.get(`${serverApi}/room-exists/${roomId}`)
